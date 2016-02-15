d8da7fb9115e6a8abe25ebc2446236557ee4ef7d
Sureya Sathiamoorthi <catchsureya@gmail.com>
2016-02-15 09:14:15 +0000

    Update parse2.py

--- parse2.py
+++ parse2.py
@@ -1,139 +1,98 @@
+
 import json
 from pymongo import MongoClient
+from pymongo import DESCENDING
 
-client = MongoClient()
 client = MongoClient('localhost', 27017)
 
-def insert():
-  input_json = """
-{
-    "Wall": [
+
+def one():
+    return {
+                "Wall_id": "wall001",
+                "user_id": "user001",
+                "message_author": "AuthorA",
+                "wall_message": "content:TRY",
+                "posted_on": "24/8/15",
+                "author_id": "A001",
+                "path_to_files": "[home/path]",
+                "wall_recepients":["user001","user002","user003","user004","user005"],
+                "w_timestamp": 10,
+                "Comments": [
+                    {
+                        "comment_id": "cmt001",
+                        "Wall_id": "wall001",
+                        "user_id": "user001",
+                        "pathtofile": "[home/doc/file]",
+                        "comment": "try 2",
+                        "profile_pic": "home2/pic.jpg",
+                        "user_name": "user2",
+                        "c_timestamp": 15
+                    }
+                ]    
+            }
+
+
+def two():
+    return {
+    "Wall_id": "wall002",
+    "user_id": "user002",
+    "message_author": "AuthorB",
+    "wall_message": "content:TRY",
+    "posted_on": "24/8/15",
+    "author_id": "A002",
+    "path_to_files": ["home/path"],
+    "wall_recepients":["user002","user003","user004","user005","user006"],
+    "w_timestamp": 678,
+    "Comments": [
         {
+            "comment_id": "cmt001",
             "Wall_id": "wall001",
             "user_id": "user001",
-            "message_author": "AuthorA",
-            "wall_message": "content:TRY",
-            "posted_on": "24/8/15",
-            "author_id": "A001",
-            "path_to_files": "[home/path]",
-            "wall_recepients":"[user001,user002,user003,user004,user005]",
-            "w_timestamp": "561651561552",
-            "Comments": [
-                {
-                    "comment_id": "cmt001",
-                    "Wall_id": "wall001",
-                    "user_id": "user001",
-                    "pathtofile": "[home/doc/file]",
-                    "comment": "try 2",
-                    "profile_pic": "home2/pic.jpg",
-                    "user_name": "user2",
-                    "c_timestamp": "8975616544515",
-                    "comment_reply": [
-                        {
-                            "reply_id": "rly001",
-                            "comment_id": "cmt001",
-                            "user_id": "user002",
-                            "pathtofile": "[home/doc/file]",
-                            "reply": "try 4",
-                            "profile_pic": "home2/pic.jpg",
-                            "user_name": "user",
-                            "r_timestamp": "91877798442158"
-                        }
-                    ]
-                },
-                {
-                    "comment_id": "cmt002",
-                    "Wall_id": "wall001",
-                    "user_id": "user002",
-                    "pathtofile": "[home/doc/file]",
-                    "comment": "try 2",
-                    "profile_pic": "home2/pic.jpg",
-                    "user_name": "user2",
-                    "c_timestamp": "6484798454984",
-                    "comment_reply": [
-                        {
-                            "reply_id": "rly002",
-                            "comment_id": "cmt002",
-                            "user_id": "user003",
-                            "pathtofile": "[home/doc/file]",
-                            "reply": "try 4",
-                            "profile_pic": "home2/pic.jpg",
-                            "user_name": "user",
-                            "r_timestamp": "198782789445"
-                        }
-                    ]
-                }
-            ]
-        },
+            "pathtofile": "[home/doc/file]",
+            "comment": "try 2",
+            "profile_pic": "home2/pic.jpg",
+            "user_name": "user2",
+            "c_timestamp": 700
+       },
         {
-            "Wall_id": "wall002",
-            "user_id": "user002",
-            "message_author": "AuthorB",
-            "wall_message": "content:TRY",
-            "posted_on": "24/8/15",
-            "author_id": "A002",
-            "path_to_files": "[home/path]",
-            "wall_recepients":"[user002,user003,user004,user005,user006]",
-            "w_timestamp": "984298429842",
-            "Comments": [
-                {
-                    "comment_id": "cmt001",
-                    "Wall_id": "wall001",
-                    "user_id": "user001",
-                    "pathtofile": "[home/doc/file]",
-                    "comment": "try 2",
-                    "profile_pic": "home2/pic.jpg",
-                    "user_name": "user2",
-                    "c_timestamp": "548494511685",
-                    "comment_reply": [
-                        {
-                            "reply_id": "rly001",
-                            "comment_id": "cmt001",
-                            "user_id": "user003",
-                            "pathtofile": "[home/doc/file]",
-                            "reply": "try 4",
-                            "profile_pic": "home2/pic.jpg",
-                            "user_name": "user",
-                            "r_timestamp": "695498414+9844"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "Wall_id": "wall003",
-            "user_id": "user003",
-            "message_author": "AuthorC",
-            "wall_message": "content:TRY",
-            "posted_on": "24/8/15",
-            "author_id": "A003",
-            "path_to_files": "[home/path]",
-            "wall_recepients":"[user003,user004,user005,user006,user007]",
-            "w_timestamp": "6549845498484"         
-        }    
+            "comment_id": "cmt001",
+            "Wall_id": "wall001",
+            "user_id": "user001",
+            "pathtofile": "[home/doc/file]",
+            "comment": "try 2",
+            "profile_pic": "home2/pic.jpg",
+            "user_name": "user2",
+            "c_timestamp": 1000
+       }
     ]
 }
-      """
 
-  return input_json
+
 
 if __name__ =="__main__":
 
-    con = MongoClient()
-
-    db = client['sampledb']
+    db = client['grad']
     post = db["post"]
 
-    wall = json.loads(insert())
-    post.insert_one(wall) 
+    insert = False
 
-    print "insert sucessful"
+    if(insert):
+        post.insert_one(one()) 
+        print "1 done"
+        
+        post.insert_one(two()) 
+        print "2 done"
+        
+    searchUser = "user002"
+    result = post.find({"wall_recepients":{"$in":[searchUser]}}).sort([["post.w_timestamp",DESCENDING],["post.Comments.c_timestamp",DESCENDING]])
+    ##result = post.find({"post.Comments.c_timestamp":{"$gt":16}})
+    seen = False    
+    for row in result:
+        seen = True
+        del row["_id"]
+        print json.dumps(row, sort_keys=True, indent=4, separators=(',', ': '))
+        print "\n\n"
 
-    recepients = db.post.distinct("Wall.wall_recepients")
-    print(recepients)
 
-    wall_time = db.post.find().sort("w_timestamp",pymongo.DECENDING)
-    print(wall_time)
-
-
-
+    if not seen:
+        print "Empty Result " 
